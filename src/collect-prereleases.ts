5c1c140790ea161335194a6f2ec7a90dc8c83215
Daniel Emery <danielremery@gmail.com>
2024-03-11 15:57:11 +0100

    Add helper to collect prereleases

---
+++ src/collect-prereleases.ts
@@ -0,0 +1,78 @@
+import { ContextArgument } from "./context";
+
+const DEFAULT_MAX_PAGE_SEARCH = 5;
+
+/**
+ * Collect all prereleases, optionally filtering out those newer than a given tag.
+ * @param context Context argument
+ * @param olderThanTag The tag to compare prereleases against.
+ * @param maxPageSearch The maximum number of pages to look back through for prereleases. Default is 5.
+ * @returns The list of prereleases and the number of newer prereleases found (and skipped).
+ */
+export async function collectPrereleases(
+  { octokit, context, logger }: ContextArgument,
+  olderThanTag?: string,
+  maxPageSearch = DEFAULT_MAX_PAGE_SEARCH
+) {
+  const { owner, repo } = context.repo;
+
+  // Collect all prereleases (within the max page limit)
+  const releasesIterator = octokit.paginate.iterator(
+    octokit.rest.repos.listReleases,
+    {
+      owner,
+      repo,
+    }
+  );
+  let prereleases: { tag_name: string; id: number; body?: string | null }[] =
+    [];
+  let currentPage = 1;
+  for await (const value of releasesIterator) {
+    logger.debug(
+      `Searching through release page #${currentPage} for prereleases`
+    );
+    prereleases = prereleases.concat(
+      value.data.filter((release) => release.prerelease)
+    );
+    if (currentPage > maxPageSearch) {
+      break;
+    }
+    currentPage++;
+  }
+
+  if (olderThanTag === undefined) {
+    return { prereleases, newerPreleaseCount: 0 };
+  }
+
+  // Determine which prereleases are older than the target release
+  let newerPreleaseCount = 0;
+  const olderPreleases = [];
+  for (const prerelease of prereleases) {
+    const diff = await octokit.rest.repos.compareCommitsWithBasehead({
+      owner,
+      repo,
+      basehead: `${prerelease.tag_name}...${olderThanTag}`,
+    });
+
+    logger.debug(
+      `Comparing ${prerelease.tag_name} and ${olderThanTag}, ahead by: ${diff.data.ahead_by}, behind by: ${diff.data.behind_by}`
+    );
+
+    if (diff.data.behind_by > 0) {
+      logger.debug(
+        `Prerelease ${prerelease.tag_name} is newer than target release, skipping`
+      );
+      newerPreleaseCount++;
+      continue;
+    } else {
+      logger.debug(
+        `Prerelease ${prerelease.tag_name} is older than target release, adding to list`
+      );
+      olderPreleases.push(prerelease);
+    }
+  }
+  return {
+    prereleases: olderPreleases,
+    skippedPreleaseCount: newerPreleaseCount,
+  };
+}
