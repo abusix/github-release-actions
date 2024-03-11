6f7c4c4d1b64ebabcdbbe98b1ac2d10612782368
Daniel Emery <danielremery@gmail.com>
2024-03-11 15:28:02 +0100

    Setup basic development environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Github Release Action Development Environment";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.bashInteractive ];
+            buildInputs = with pkgs; [
+              nodejs_20
+            ];
+          };
+        });
+    };
+}
